1d483662de3638fbbd243fac57ef39566968a206
Jana <click2jana21@gmail.com>
2025-12-31 15:40:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Delete alternate characters/delete-alternate-characters.java
@@ -0,0 +1,14 @@
+// User function template for Java
+
+class Solution {
+    static String delAlternate(String S) {
+        // code here
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<S.length();i++){
+            if(i%2==0){
+                sb.append(S.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
