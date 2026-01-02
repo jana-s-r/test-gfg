f98ab8382970b0866d4a28e0098f7e94de0d1784
Jana <click2jana21@gmail.com>
2026-01-02 18:15:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,43 +1,17 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            Solution ob = new Solution();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public int getSecondLargest(int[] arr) {
         // code here
-        int largest = -1;
-        int secondLargest = -1;
+        Arrays.sort(arr);
+        int res =0;
+        int l = arr[arr.length-1];
         
-        for (int num : arr) {
-            if(num > largest){
-                secondLargest = largest;
-                largest = num;
-            }else if(num < largest && num > secondLargest){
-                secondLargest = num;
+        for(int i=arr.length-2;i>=0;i--) {
+            if(l!=arr[i]){
+                res = arr[i];
+                break;
             }
         }
-        return secondLargest;
+        
+        return res;
     }
 }
